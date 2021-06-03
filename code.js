98c9627d5481886dd5d2881849a9c51775411380
Telvin Mathews <telvinmathews@gmail.com>
2021-06-03 08:28:34 -0500

    unit tests for edge cases

--- code.js
+++ code.js
@@ -3,6 +3,10 @@ function helloWorld() {
     return "Hello, World!";
 }
 
-function sayHello(name) {
-    return "Hello, " + name + "!";
+function sayHello(input) {
+    if (input === undefined || input === true || input === false || input === null || input === '' || input === typeof 3 || input === typeof [] || input === typeof "3") {
+        return "Hello, World!";
+    } else {
+        return "Hello, " + input + "!";
+    }
 }
--- tests.js
+++ tests.js
@@ -19,10 +19,45 @@ describe('sayHello', function() {
         expect(typeof sayHello).toBe('function');
     });
     it('should return a string when called', function() {
-        expect(typeof sayHello("Pat")).toBe("string");
+        expect(typeof sayHello()).toBe("string");
+    });
+    it('should return the string "Hello, Jane!" when executed', function() {
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
+    });
+    it('should return the string "Hello, Alex!" when executed', function() {
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
     });
     it('should return the string "Hello, Pat!" when executed', function() {
         expect(sayHello("Pat")).toBe("Hello, Pat!");
     });
+    it('should return the string "Hello, World!" when executed', function() {
+        expect(sayHello()).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World!" when executed', function() {
+        expect(sayHello(true)).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World!" when executed', function() {
+        expect(sayHello(false)).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World!" when executed', function() {
+        expect(sayHello(null)).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World!" when executed', function() {
+        expect(sayHello(typeof 3.5)).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World!" when executed', function() {
+        expect(sayHello(typeof 3)).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World!" when executed', function() {
+        expect(sayHello(typeof "3")).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World!" when executed', function() {
+        expect(sayHello(typeof '')).toBe("Hello, World!");
+    });
 });
 
+//the input is null?
+// the input is an empty string like ""?
+// the input is a number like 2.3?
+// the input is a number inside a string like "5"?
+// the input is another data type like an array, object, or function?
